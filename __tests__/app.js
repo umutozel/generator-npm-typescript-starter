4b286c8ab743140accccd5a6df80fe8dab7538a9
umutozel <umutozel@gmail.com>
2018-07-22 01:30:10 +0300

    Improve tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,14 +3,32 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+const name = 'test-project';
+const description = 'description of test project';
+const username = 'alanturing';
+const fullname = 'Alan Turing';
+
 describe('generator-npm-typescript-starter:app', () => {
   beforeAll(() => {
     return helpers
       .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+      .withPrompts({ name, description, username, fullname });
   });
 
   it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+    assert.file([
+      'LICENSE',
+      'package.json',
+      'README.md',
+      '.gitignore',
+      '.nycrc',
+      'index.ts',
+      'mocha.opts',
+      'tsconfig.json',
+      'tslint.json',
+      'lib/index.ts',
+      'test/index.spec.ts',
+      '.vscode/settings.json'
+    ]);
   });
 });
